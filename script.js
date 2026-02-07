b2c5e445173da2e30b05af2f3beeedffa5ab7b76
g-akca <gok.akca66@gmail.com>
2026-02-07 16:14:04 +0300

    Add JS logic to change the number of squares

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     squareContainer.appendChild(square);
 }
+
+popupButton.addEventListener("click", () => {
+    const numOfSquares = prompt("Enter the number of squares per side for the new grid (max. 100):");
+    if (numOfSquares > 100 || numOfSquares < 0) {
+        alert("Please enter a number between 0 and 100!");
+        return;
+    }
+
+    squareContainer.innerHTML = "";
+    const squareSize = squareContainer.clientWidth / numOfSquares;
+
+    for (let i = 0; i < numOfSquares * numOfSquares; i++) {
+        const square = document.createElement("div");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        squareContainer.appendChild(square);
+    }
+});
